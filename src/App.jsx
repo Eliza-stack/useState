f0c1b2206e8d5dbd1ae2655b03110f2c899913c8
Eliza Atanbaeva <bknade794@gmail.com>
2025-03-22 20:28:15 +0600

    Pets

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
+import style from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
-
-  const increment = () => {
-    setCount1(count1 + 1);
-  }
+  const pets = [
+    {
+      name: "Kitty",
+      image:
+        "https://i.pinimg.com/474x/4a/73/d3/4a73d39983123d16812300c2f350c4eb.jpg",
+    },
+    {
+      name: "Puppy",
+      image:
+        "https://i.pinimg.com/474x/21/2f/c8/212fc8c86b4b30cdaff1e9e1d7ae377a.jpg",
+    },
+  ];
 
   return (
     <>
-    <h1>Метод 1</h1>
-    <h2> лайк: {count}</h2>
-    <button onClick={()=> setCount(count+1)}>+1</button>
-
-    <h1>Метод 2</h1>
-    <h2> лайк: {count1}</h2>
-    <button onClick={increment}>+1</button>
-      
-      
+      {pets.map((pet, index) => (
+        <div key={pet.index} className={style.box}>
+          <div className={style.image}>
+            <img src={pet.image} alt={pet.name} />
+          </div>
+          <h2> {pet.name} +1</h2>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/map.js
@@ -0,0 +1,5 @@
+const lists = [1,2,3,4,5,6,7,8,9,10];
+
+const listsItems = lists.map(list => list*2)
+
+console.log(listsItems); 
